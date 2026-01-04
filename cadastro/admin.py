711cfe7833f5bb089a1ec10f756ab850c1470cc7
Seu Nome <seuemail@dominio.com>
2026-01-04 14:17:30 -0500

    formulario de cadastro somente externo

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Cliente
 
-admin.site.register(Cliente)
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'criado_em') # Mostra essas colunas na lista
+    search_fields = ('nome',) # Adiciona uma barra de busca por nome
+
 # Register your models here.
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Cliente
+
+def cadastrar_cliente(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+
+        Cliente.objects.create(nome=nome, email=email)
+        return redirect('admin:index')
+    
+    return render(request, 'form_cadastro.html')
 
 # Create your views here.
